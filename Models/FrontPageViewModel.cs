8ab59f32380bf46ef5856d98395f5c7766c31fdc
Ashley Velasquez <AshleyJVelasquez@outlook.com>
2020-06-07 22:33:31 -0700

    Updated my TODOs and Added Methods to create

--- Models/FrontPageViewModel.cs
+++ Models/FrontPageViewModel.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Printful_Library.Models;
+using Printful_Library.Printful;
 
 namespace Ashley_MockUp_Store.Models
 {
-    public static class FrontPageViewModel
+    public class FrontPageViewModel
     {
-        public static List<Product> ReturnProductList(List<SyncProduct> total)
+        public List<Product> ReturnProductList(List<SyncProduct> total)
         {
             List<Product> products = new List<Product>();
             
@@ -20,19 +21,36 @@ namespace Ashley_MockUp_Store.Models
                     ImageURL = product.thumbnail_url, 
                     Name = product.name 
                 };
-                item.Price = GetProductPrice();
-                item.NumberOfSizes = GetProductSizes();
+                //item.Price = GetProductPrice(item.ID);
+                //item.NumberOfSizes = GetProductSizes();
+                //TODO 06-07-2020 Finish this wishful programming
+                ProductTotalInfo details = ReturnProductDetails(product.id);
+                item.Price = GetProductPrice(details);
+                item.NumberOfSizes = GetProductSizes(details);
             }
             return products;
         }
+
+        //TODO 06-07-2020 Returning only the class
+        public ProductTotalInfo ReturnProductDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        //TODO 06/07/2020 How do I get all the product info? 
+        public async Task<ProductTotalInfo> GetProductDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         //TODO Method on getting Price
         //06-03-2020 Let's get by with STARTING PRICE AT $$$$
-        public static float GetProductPrice()
+        public float GetProductPrice(ProductTotalInfo details)
         {
             throw new NotImplementedException();
         }
         //TODO Method on getting Sizes
-        public static int GetProductSizes()
+        public int GetProductSizes(ProductTotalInfo details)
         {
             throw new NotImplementedException();
         }
--- Models/IFrontPageViewModel.cs
+++ Models/IFrontPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Printful_Library.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,7 +8,8 @@ namespace Ashley_MockUp_Store.Models
 {
     public interface IFrontPageViewModel
     {
-        //TODO 06-03-2020 Do I still need this?
+        //TODO 06-03-2020 Do I still need this? Clarification, do I still need this interface when I have a separate products list? 
         public List<Product> products{ get; set; }
+        public List<Product> ReturnProductList(List<SyncProduct> total);
     }
 }
